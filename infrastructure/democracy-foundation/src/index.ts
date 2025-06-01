315024790e68d73f9555070eaab048d1ea3257d0
Manuel Ruck <git@manuelruck.de>
2025-06-01 17:44:22 +0200

    refactor: Simplify imports and exports in index.ts for better readability

--- infrastructure/democracy-foundation/src/index.ts
+++ infrastructure/democracy-foundation/src/index.ts
@@ -1,25 +1,40 @@
 import * as pulumi from "@pulumi/pulumi";
-import { vpcOutputs } from "./vpcs";
-import { firewallOutputs } from "./firewalls";
-import { bundestagIoOutputs } from "./domains/bundestag-io";
-import { democracyDeutschlandDeOutputs } from "./domains/democracy-deutschland-de";
-import { democracyAppDeOutputs } from "./domains/democracy-app-de";
+import {
+  vpcOutputs,
+  defaultFra1Vpc,
+  websiteVpc,
+  kubernetesTestVpc,
+} from "./vpcs";
+import {
+  firewallOutputs,
+  k8sPublicAccessFirewall,
+  k8sWorkerFirewall,
+} from "./firewalls";
+import { bundestagIoOutputs, bundestagIo } from "./domains/bundestag-io";
+import {
+  democracyDeutschlandDeOutputs,
+  democracyDeutschlandDe,
+} from "./domains/democracy-deutschland-de";
+import {
+  democracyAppDeOutputs,
+  democracyAppDe,
+} from "./domains/democracy-app-de";
 import { platformOutputs } from "./platform-stack-refs";
 import { exportStackHealth } from "../../shared/src/monitoring";
 
 // Collect all resources for monitoring
 const allResources = [
   // VPCs
-  require("./vpcs").defaultFra1Vpc,
-  require("./vpcs").websiteVpc,
-  require("./vpcs").kubernetesTestVpc,
+  defaultFra1Vpc,
+  websiteVpc,
+  kubernetesTestVpc,
   // Firewalls
-  require("./firewalls").k8sPublicAccessFirewall,
-  require("./firewalls").k8sWorkerFirewall,
+  k8sPublicAccessFirewall,
+  k8sWorkerFirewall,
   // Domains
-  require("./domains/bundestag-io").bundestagIo,
-  require("./domains/democracy-deutschland-de").democracyDeutschlandDe,
-  require("./domains/democracy-app-de").democracyAppDe,
+  bundestagIo,
+  democracyDeutschlandDe,
+  democracyAppDe,
 ];
 
 // Export stack health monitoring
@@ -34,8 +49,8 @@ export {
 };
 
 // Export individual resources for easier reference
-export { defaultFra1Vpc, websiteVpc, kubernetesTestVpc } from "./vpcs";
-export { k8sPublicAccessFirewall, k8sWorkerFirewall } from "./firewalls";
-export { bundestagIo } from "./domains/bundestag-io";
-export { democracyDeutschlandDe } from "./domains/democracy-deutschland-de";
-export { democracyAppDe } from "./domains/democracy-app-de";
+export { defaultFra1Vpc, websiteVpc, kubernetesTestVpc };
+export { k8sPublicAccessFirewall, k8sWorkerFirewall };
+export { bundestagIo };
+export { democracyDeutschlandDe };
+export { democracyAppDe };
